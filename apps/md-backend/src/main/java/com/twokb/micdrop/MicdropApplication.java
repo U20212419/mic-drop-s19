7fa5ede18730fb53ab09a793dc0c6326d9b27bd6
U20212419 <a20212419@pucp.edu.pe>
2026-03-20 02:46:27 -0500

    Base monorepo

---
+++ apps/md-backend/src/main/java/com/twokb/micdrop/MicdropApplication.java
@@ -0,0 +1,13 @@
+package com.twokb.micdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicdropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicdropApplication.class, args);
+	}
+
+}
---
+++ apps/md-backend/src/test/java/com/twokb/micdrop/MicdropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twokb.micdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicdropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
